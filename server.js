de2a553cf97337787cb03444526f363c88b01e6d
amandeep <amandeepkaursekhon9@gmail.com>
2023-11-02 19:37:18 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+connectDB();
+
+app.use(express.json({ extended: false }));
+
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/users', require('./routes/api/users'));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
